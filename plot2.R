b65e136ecb1ea469d8ec6f4355ee5d65f5028a1e
lyonnce <costomsl@gmail.com>
2018-02-02 14:55:11 -0400

    Delete plot2.R

--- plot2.R
+++
@@ -1,9 +0,0 @@
-
-png(filename = '~/Desktop/plot2.png', width = 480, height = 480, units='px')
-w2$datehour = as.POSIXct(paste(w2$Date ,w2$Time),
-                             format = "%Y-%m-%d %H:%M:%S")
-plot(y = w2$Global_active_power, x = w2$datehour, type = "l", 
-
-     ylab = "Global Active Power (kilowatts)", xlab = ""
-     )
-dev.off()
